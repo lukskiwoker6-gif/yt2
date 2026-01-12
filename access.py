089f8680bd2f55aabc1287b7011ca80b65615aba
lukskiwoker6-gif <lukskiwoker6@gmail.com>
2026-01-12 05:52:31 +0000

    Update access.py

--- access.py
+++ access.py
@@ -1,15 +1,19 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-from config import CHANNEL_USERNAME
+from config import CHANNEL_USERNAME, ADMIN_IDS
 from database import is_paid
 
 
-async def has_access(update: Update, context: ContextTypes.DEFAULT_TYPE) -> bool:
+async def has_access(update, context) -> bool:
     user_id = update.effective_user.id
 
+    # ✅ АДМИН — ВСЕГДА ДОСТУП
+    if user_id in ADMIN_IDS:
+        return True
+
+    # 💰 ПЛАТНЫЙ — ДОСТУП
     if is_paid(user_id):
         return True
 
+    # 🔒 ПРОВЕРКА ПОДПИСКИ
     try:
         member = await context.bot.get_chat_member(
             CHANNEL_USERNAME,
