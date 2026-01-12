910683b57fe89263301a14d4a30f3253230897db
lukskiwoker6-gif <lukskiwoker6@gmail.com>
2026-01-12 05:28:47 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,10 @@
-import os
+BOT_TOKEN = "8228248528:AAFc0uNIquzw2U9eTz42XyTD_jCudupyO-U"
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8228248528:AAFc0uNIquzw2U9eTz42XyTD_jCudupyO-U")
-
-# 👉 ВСТАВЬ СВОЙ TELEGRAM ID
 ADMIN_IDS = [1436019784]
 
-# YouTube cookies
-COOKIES_FILE = "cookies.txt"
+CHANNEL_USERNAME = "@your_channel"   # без https
+STARS_PRICE = 299                    # ⭐
 
-# папка для загрузок
 DOWNLOAD_DIR = "downloads"
-
-# максимальный размер для отправки как video (Telegram Bot API)
-TG_VIDEO_LIMIT = 2 * 1024 * 1024 * 1024  # 2GB
+COOKIES_FILE = "cookies.txt"
+ARIA2_PATH = "/usr/bin/aria2c"
